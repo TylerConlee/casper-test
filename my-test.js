5651dcf2de269e17e1564c9998965151aab09b84
Tyler Conlee <tylerlconlee@gmail.com>
2017-04-03 07:57:50 -0700

    adjusted test

--- my-test.js
+++ my-test.js
@@ -2,7 +2,7 @@ casper.options.viewportSize = {width: 1024, height: 768};
 casper.test.begin("Testing Button", 1, function buttonTest(test) {
     casper.start("https://serene-stream-84316.herokuapp.com/", function() {
     	//Click Button
-    	casper.click('input[id="button"]');
+    	casper.click('#button');
     	test.assertSelectorHasText('#test', 'Some Text');
     }).run(function() {
         test.done();
