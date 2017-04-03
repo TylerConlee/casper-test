cc790fab4e2e3daa85781d1eeafa62f8ce0d5dea
Tyler Conlee <tylerlconlee@gmail.com>
2017-04-03 07:13:59 -0700

    init commmit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sample Page</title>
+</head>
+<body>
+<input type="button" name="click" title="Click" onclick="showText();" value="Click the button" />
+</body>
+</html>
+<?php ; ?>
